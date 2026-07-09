500bfb9c999fe211f49a573fcaf6814c1678651a
simonmenard1738 <poisonedcheeto@gmail.com>
2026-07-09 18:31:15 -0400

    Slight fix
    I forgot to kill "value" too

--- src/main/webapp/plugins/linter/linter.js
+++ src/main/webapp/plugins/linter/linter.js
@@ -186,7 +186,7 @@ var LinterWindow = function (editorUi, x, y, w, h) {
 	// Header
 	const header = document.createElement('tr');
 
-	['Setting', 'Enabled', 'Value'].forEach(function (text) {
+	['Setting', 'Enabled'].forEach(function (text) {
 		const th = document.createElement('th');
 		th.style.textAlign = 'left';
 		th.style.padding = '4px 8px';
